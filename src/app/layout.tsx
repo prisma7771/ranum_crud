35f7dd3c13c9ba7867245b12ca2bffaa413df42e
prisma7771 <prismaputra91@gmail.com>
2025-04-19 18:39:15 +0700

    update dashboard

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import DashboardShell from "@/components/layouts/dashboard/dashboardLayout";
-import { AppDataProvider, PaketsProvider } from "@/context/AppContext";
+import { AppDataProvider } from "@/context/AppContext";
 
 const inter = Inter({ subsets: ["latin"] });
 
